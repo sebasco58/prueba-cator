d1720c5ba23546d81bf3f6c82080908a9edb28b0
sebasco58 <jscastano56@misena.edu.co>
2021-03-16 11:02:31 -0500

    changes home

--- resources/js/pages/Home.js
+++ resources/js/pages/Home.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import { getAllCommittes, getAllStimulus } from '../containers/User';
+import { getAll} from '../containers/LearnerNovelties';
 import LineChart from '../components/LineChart';
 import DoughnutChart from '../components/DoughnutChart';
+import { Link } from "react-router-dom";
+import moment from 'moment';
+import DataTable from '../components/DataTable';
 
 class Home extends Component {
     constructor(props) {
@@ -9,12 +13,41 @@ class Home extends Component {
         this.state = {
             allCommittes: [],
             allStimulus: [],
+            allFormativeMeasures: [],
+            formativemeasures: [],
+            year: null,
+            month: null,
+            day: null,
+            allVoluntaryRetirement: [],
+            allAcademic: [],
+            allDisciplinary: [],
+            allAcademicDisciplinary: [],
+            allRetirement: [],
         }
     }
 
     async getCommittes() {
         let data = await getAllCommittes();
-        this.setState({ allCommittes: data });
+        let academic = data.filter(sesion => sesion.infringement_type.name == "ACADEMICA");
+        this.setState({ allAcademic: academic.length });
+        let disciplinary = data.filter(sesion => sesion.infringement_type.name == "DISCIPLINARIA");
+        this.setState({ allDisciplinary: disciplinary.length});
+        let formativeMeasure = data.filter(session => session.responsibles.length > 0);
+        this.setState({ allFormativeMeasures: formativeMeasure });
+
+        // var f = new Date();
+        // this.setState({ year: f.getFullYear() });
+        // this.setState({ month: f.getMonth() +1 });
+        // this.setState({ day: f.getDate() });
+        // console.log(f.getFullYear() + "-" + (f.getMonth() +1) + "-" + f.getDate());
+        // this.setState({ dateToday: f.getFullYear() + "-" + (f.getMonth() +1) + "-" + f.getDate() });
+
+        $('#tableFormative').DataTable({
+            language:{
+                url: 'https://cdn.datatables.net/plug-ins/1.10.21/i18n/Spanish.json'
+            },
+            info: false,
+        });
     }
 
     async getStimulus() {
@@ -22,9 +55,18 @@ class Home extends Component {
         this.setState({ allStimulus: data });
     }
 
+    async getLearnerNovelties () {
+        let data = await getAll();
+        let retirement = data.filter(noveltie => noveltie.novelty_type.name == "RETIRO");
+        this.setState({allRetirement: retirement.length});
+        let voluntaryretirement = data.filter(noveltie => noveltie.novelty_type.name == "RETIRO VOLUNTARIO");
+        this.setState({allVoluntaryRetirement: voluntaryretirement.length});
+    }
+
     componentDidMount() {
         this.getCommittes();
         this.getStimulus();
+        this.getLearnerNovelties();
     }
 
     render() {
@@ -32,55 +74,115 @@ class Home extends Component {
             <>
                 <div className="row">
                     <div className="col">
-                        <h3>Dashboard</h3>
+                        <h2>Dashboard</h2>
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-12 col-lg-6 mb-1">
+                    <div className="col-12 col-lg-3 mt-3">
                         <div className="card">
                             <div className="card-body">
-                                <h5>Aprendices en comité</h5>
-                                <LineChart
-                                    label={'Aprendices en comite'}
-                                    color={'rgba(255, 99, 132, 0.2)'}
-                                />
+                                <h5>Total casos academicos</h5>
+                                <p className="text-center">{this.state.allAcademic}</p>
                             </div>
                         </div>
                     </div>
-                    <div className="col-12 col-lg-6">
+                    <div className="col-12 col-lg-3 mt-3">
                         <div className="card">
                             <div className="card-body">
-                                <h5>Comites por tipo de falta</h5>
-                                <DoughnutChart
-                                    label=""
-                                />
+                                <h5>Total casos disciplinarios</h5>
+                                <p className="text-center">{this.state.allDisciplinary}</p>
                             </div>
                         </div>
-
+                    </div>
+                    <div className="col-12 col-lg-3 mt-3">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5>Total retiros</h5>
+                                <p className="text-center">{this.state.allRetirement}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-12 col-lg-3 mt-3">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5>Total retiros voluntarios</h5>
+                                <p className="text-center">{this.state.allVoluntaryRetirement}</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="row mt-1">
-                    <div className="col-12 col-lg-6 mb-1">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5>Comites por tipo de falta</h5>
-                                <DoughnutChart
-                                    label=""
-                                />
-                            </div>
-                        </div>
-
-                    </div>
-                    <div className="col-12 col-lg-6">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5>Aprendices por sus estimulos</h5>
-                                <LineChart
-                                    label={'Aprendices por estimulos'}
-                                    color={'rgba(54, 162, 235, 0.2)'}
-                                />
-                            </div>
-                        </div>
+                <hr/>
+                <div className="row my-4">
+                    <div className="col-12">
+                        <table className="display table table-hover table-sm" id="tableFormative">
+                            <thead>
+                                <tr>
+                                    <th className="hide">Fecha comité</th>
+                                    <th>Aprendiz</th>
+                                    <th>Medida formativa</th>
+                                    <th className="hide">Responsable</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.state.allFormativeMeasures.length > 0 ? (
+                                    this.state.allFormativeMeasures.map(allFormativeMeasure =>(
+                                        <tr key={allFormativeMeasure.id}>
+                                            <td>
+                                                <Link to={"/app/committees/" + allFormativeMeasure.committee.id}>
+                                                            {allFormativeMeasure.committee.date}
+                                                </Link>
+                                            </td>
+                                            <td>{allFormativeMeasure.learner.name}</td>
+                                            <td>{allFormativeMeasure.responsibles ? (
+                                                    allFormativeMeasure.responsibles.length <= 1 ? (
+                                                        allFormativeMeasure.responsibles.map(responsible => (
+                                                            `${responsible.pivot.formative_measure.name}`
+                                                        ))
+                                                    ) : (
+                                                            <p>
+                                                                {allFormativeMeasure.responsibles[0].username}
+                                                                <button
+                                                                type="button"
+                                                                onMouseOver={this.tooltip}
+                                                                className="btn btn-link btn-sm"
+                                                                data-toggle="tooltip"
+                                                                data-placement="right"
+                                                                title="Hay mas de una medida formativa"
+                                                                >
+                                                                    <i className="fa fa-info-circle text-info" aria-hidden="true"></i>
+                                                                </button>
+                                                            </p>
+                                                        )      
+                                                ) : (
+                                                    'No hay medida'
+                                                )}
+                                                </td>
+                                                <td className="hide">{allFormativeMeasure.responsibles ? (
+                                                        allFormativeMeasure.responsibles.length <= 1 ? (
+                                                            allFormativeMeasure.responsibles.map(responsible => (
+                                                                `${responsible.username}`
+                                                            ))
+                                                    ) : (
+                                                            <>
+                                                            <p>
+                                                                {allFormativeMeasure.responsibles[0].username}
+                                                            </p>
+                                                            </>
+                                                        )
+                                                ) : (
+                                                    'No hay Resposables'
+                                                )}
+                                                </td>
+                                        </tr>
+                                        )
+                                    )
+                                ) : (
+                                    <tr className="col">
+                                        <td>No hay datos disponibles</td>
+                                    </tr>
+                                )}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </>
